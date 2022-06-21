50525bae118c18f811fe4924098c9f92bd848d6f
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-06-21 17:42:34 +0900

    header追加

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Text } from "@chakra-ui/react";
+
+export const Header = () => {
+  return (
+    <header style={{ backgroundColor: "black" }}>
+      <Text fontWeight="bold" fontSize="x-large" color="gray.200">
+        らんまるの部屋
+      </Text>
+    </header>
+  );
+};
